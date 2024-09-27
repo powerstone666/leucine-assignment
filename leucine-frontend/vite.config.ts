3a393dc259db4c5745932f2b2c2542178d2ac5ba
imran pasha <imranpasha8225@gmail.com>
2024-09-27 19:57:23 +0530

    updated link

--- leucine-frontend/vite.config.ts
+++ leucine-frontend/vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist'  // Ensure this matches the distDir in vercel.json
+  }
 });
